5cf84ff1ee4b1cd886259df90e20502744a9b3a5
Tromesh Chalodiya <chalodiyatromesh@yahoo.com>
2019-06-17 16:37:13 -0500

    Diff season using the react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SeasonDisply from './SeasonDisply'
+import Spinner from './Spinner'
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        //this is the only time we do direct assignment to this.state
+        this.state = { lat: null, errMessage: '' }
+    }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    // state = { lat: null, errMessage: '' }
+    //another method to define the state is remove the constructor method
+    //directly write the state as state = {lat: null, errMessage: ''}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    componentDidMount() {
+        console.log('componentDidMount render')
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude}),
+            err => this.setState({ errMessage: err.message }) 
+        )
+    } 
+
+    renderContent() {
+        if(this.state.errMessage && !this.state.lat){
+            return <div>Error: {this.state.errMessage}</div>
+        }
+
+        if(!this.state.errMessage && this.state.lat){
+            return <SeasonDisply lat={this.state.lat}/>
+        }
+
+        return <Spinner message="Please accept location request"/>   
+    }
+
+    //React says we have to define render!!
+    render() {
+        return <div className="border red">{this.renderContent()}</div>
+    }
+}
+ReactDOM.render(<App />, document.querySelector('#root'))
